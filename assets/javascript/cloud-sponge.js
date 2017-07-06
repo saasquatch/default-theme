baead3a06541f3ac4b876a515d22831401c05108
jemason <eric.mason@yupiq.com>
2017-07-06 16:26:36 -0700

    EM handle imported contacts appropriately

--- assets/javascript/cloud-sponge.js
+++ assets/javascript/cloud-sponge.js
@@ -1,4 +1,10 @@
+function handleImportedContacts(contacts, source, owner) {
+    var emailList = document.getElementById('contact-list');
+    emailList.oninput();
+}
+
 cloudsponge.init({
     skipSourceMenu: true,
+    afterSubmitContacts: handleImportedContacts,
     sources : ["gmail", "yahoo", "windowslive"]
 });
